8e1636f7dbee3e7d621258b3cd8273ca4c3fa22b
David Zhang <cajan2@163.com>
2018-04-18 00:50:47 +0800

    ch4 oo finished

---
+++ tree/entry/entry.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ghjan/learngo/tree"
+	"golang.org/x/tools/container/intsets"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
+
+func main() {
+	var root tree.Node
+	root = tree.Node{Value: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{5, nil, nil}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = tree.CreateNode(2)
+
+	testNode(root)
+	fmt.Println("\n- myTreeNode postOrder----")
+	mynode := myTreeNode{&root}
+	mynode.postOrder()
+	fmt.Println("\n- testSparser----")
+	testSparse()
+
+}
+
+func testNode(root tree.Node) {
+
+	fmt.Println(root)
+	//nodes := []Node{
+	//  {value: 3},
+	//  {},
+	//  {6, nil, &root},
+	//}
+	//fmt.Println(nodes)
+
+	root.Print()
+	fmt.Println()
+	root.Right.Left.Print()
+	fmt.Println()
+	root.Right.Left.SetValue(4)
+	root.Right.Left.Print()
+	fmt.Println()
+
+	root.SetValue(100)
+	root.Print()
+	fmt.Println()
+	pRoot := &root
+	pRoot.Print()
+	pRoot.SetValue(200)
+	pRoot.Print()
+
+	fmt.Println("\n-nil---")
+	var pRoot2 *tree.Node
+	pRoot2.SetValue(200)
+	pRoot2.Print()
+	pRoot2 = &root
+	pRoot2.SetValue(300)
+	pRoot2.Print()
+	fmt.Println("\n-Traverse---")
+	pRoot.SetValue(3)
+	pRoot.Right.Left.SetValue(4)
+	pRoot.Traverse()
+}
+
+func testSparse() {
+	s := intsets.Sparse{}
+	s.Insert(1)
+	s.Insert(1000)
+	s.Insert(1000000)
+	fmt.Println(s.Has(1000))
+	fmt.Println(s.Has(100000))
+
+}

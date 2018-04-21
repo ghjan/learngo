c96c4bb91c047544c73b5fb05915900bae2cb8f2
David Zhang <cajan2@163.com>
2018-04-21 18:13:22 +0800

    不用默认Client，自定义一个client 使用CheckRedirect

--- http/client.go
+++ http/client.go
@@ -6,13 +6,19 @@ import (
 	"net/http/httputil"
 )
 
-const UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) " + " AppleWebKit/602.1.50 (KHTML, like Gecko) " + " CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1"
+const userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) " + " AppleWebKit/602.1.50 (KHTML, like Gecko) " + " CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1"
 
 func main() {
 	request, err := http.NewRequest(http.MethodGet, "http://www.imooc.com", nil)
-	request.Header.Add("User-Agent", UserAgent)
+	request.Header.Add("User-Agent", userAgent)
 
-	resp, err := http.DefaultClient.Do(request)
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect:", req)
+			return nil
+		},
+	}
+	resp, err := client.Do(request)
 	if err != nil {
 		panic(err)
 	}

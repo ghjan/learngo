6b9629262f777a2480daaba8a13153df6b77266c
David Zhang <cajan2@163.com>
2018-04-28 20:24:43 +0800

    rpcclient

--- crawer/rpc/rpc.go
+++ crawer/rpc/rpc.go
@@ -10,6 +10,9 @@ type Args struct {
 	A, B int
 }
 
+//{"method":"abc.ef"}
+//{"method":"DemoService.Div", "params":[{"A":3, "B":4}], "id":1}
+//{"method":"DemoService.Div", "params":[{"A":3, "B":0}], "id":1234}
 func (DemoService) Div(args Args, result *float64) error {
 	if args.B == 0 {
 		return errors.New("division by zero")

d5f2f4feedf895737256f2055c53428904c89375
David Zhang <cajan2@163.com>
2018-04-28 23:30:38 +0800

    add some log

--- crawler_distributed/persist/rpc.go
+++ crawler_distributed/persist/rpc.go
@@ -1,6 +1,8 @@
 package persist
 
 import (
+	"log"
+
 	"github.com/ghjan/learngo/crawler/engine"
 	persistReal "github.com/ghjan/learngo/crawler/persist"
 	"gopkg.in/olivere/elastic.v5"
@@ -14,7 +16,10 @@ type ItemSaverService struct {
 func (s *ItemSaverService) Save(item engine.Item, result *string) error {
 	err := persistReal.Save(s.Client, item, s.Index)
 	if err == nil {
+		log.Printf("Item %v saved ", item)
 		*result = "ok"
+	} else {
+		log.Printf("Error saving %v: %v", item, err)
 	}
 	return err
 }

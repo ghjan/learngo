2ee37a474dacf12a60edd3f3623c26f0aafc2c4f
David Zhang <cajan2@163.com>
2018-04-30 01:01:57 +0800

    rateLimiter computed from a Qps

--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -13,21 +13,21 @@ import (
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 	"time"
+	"github.com/ghjan/learngo/crawler/config"
 )
 
-var rateLimiter = time.Tick(5 * time.Millisecond)
+var rateLimiter = time.Tick(time.Second / config.Qps)
 
 func Fetch(url string) ([]byte, error) {
-	//提交请求
+	<-rateLimiter
+	//创建client和req
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 
 	//增加header选项
 	req.Header.Add("Cookie", "Hm_lvt_2c8ad67df9e787ad29dbd54ee608f5d2=1524713461; sid=A1218s9FBBOVwXcCrafX; ipCityCode=10103000; ipOfflineCityCode=10103000; JSESSIONID=abczR5SeLN_i86H6M-amw; Hm_lpvt_2c8ad67df9e787ad29dbd54ee608f5d2=1524737141")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36")
-
-	<-rateLimiter
-	//resp, err := http.Get(url)
+	//提交请求
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
